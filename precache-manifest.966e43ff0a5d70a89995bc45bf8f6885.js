92cd23fdd5d10e38093e4160bdc8f91dba587ac9
Matt M <mattems@live.com>
2020-09-27 16:53:30 -0500

    Updates

---
+++ precache-manifest.966e43ff0a5d70a89995bc45bf8f6885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9178446e550eb68a003ca28f142ece9a",
+    "url": "/mattems12/stacker-puzzle/index.html"
+  },
+  {
+    "revision": "e82c2b81bf8b22d33746",
+    "url": "/mattems12/stacker-puzzle/static/css/main.57c584d2.chunk.css"
+  },
+  {
+    "revision": "e6456dfd5a6614f0ec86",
+    "url": "/mattems12/stacker-puzzle/static/js/2.abbf4e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mattems12/stacker-puzzle/static/js/2.abbf4e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82c2b81bf8b22d33746",
+    "url": "/mattems12/stacker-puzzle/static/js/main.01513848.chunk.js"
+  },
+  {
+    "revision": "e64a458d1ecde5b3a786",
+    "url": "/mattems12/stacker-puzzle/static/js/runtime-main.106afc7c.js"
+  }
+]);
